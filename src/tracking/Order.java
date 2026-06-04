548de42e681e2f839df7b47bf3fc4852b26de2d4
bridg <bridg@Bree>
2026-06-04 12:08:12 +0000

    Initial commit

---
+++ src/tracking/Order.java
@@ -0,0 +1,33 @@
+package tracking;
+
+public class Order {
+
+    private int orderId;
+    private int TableNo;
+    private OrderStatus status;
+
+    public Order(int orderId,
+    		int TableNo,
+                 OrderStatus status) {
+
+        this.orderId = orderId;
+        this.TableNo = TableNo;
+        this.status = status;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public int getTableNo() {
+        return TableNo;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
